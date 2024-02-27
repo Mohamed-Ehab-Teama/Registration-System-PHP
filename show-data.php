97b7c197be14fcd4f33d4d7ccd9e5f8c55a14d7b
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2024-02-27 22:47:42 +0200

    Just Upload files

---
+++ show-data.php
@@ -0,0 +1,28 @@
+<?php include 'inc/header.php' ?>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="text-center display-4 border my-5">Data</h1>
+            <div>
+                <h2>Name :</h2>
+                <h2>Email :</h2>
+                <h2>Phone :</h2>
+                <h2>Address :</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'inc/footer.php' ?>
