d0b6946c37041062b7944f6356e7af98e2b3ac02
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2024-02-29 22:25:09 +0200

    Day2 on the project

---
+++ inc/db.php
@@ -0,0 +1,10 @@
+<?php
+
+try {
+    $dsn = "mysql:host=localhost;dbname=auth_php";
+
+    $pdo = new PDO($dsn, "root", "");
+} catch (PDOException $e) {
+    echo "ERROR : " . $e->getMessage();
+    die();
+}
